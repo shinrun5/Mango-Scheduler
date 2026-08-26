4e4ba0906209974f74160b53770d071137e9c580
shinrun5 <321danielhe@gmail.com>
2026-08-26 01:49:00 -0400

    finished get/post function for employee and store

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,10 +3,37 @@ import prisma from './lib/prisma.js';
 
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+
+app.post('/employees', async (req, res) => {
+  const { name, hourLimit} = req.body;
+  const newEmployee = await prisma.employee.create({
+    data: {
+      name:  name,
+      hourLimit: hourLimit,
+    },
+  });
+  res.json(newEmployee);
+});
 
 app.get('/employees', async (req, res) => {
   const employees = await prisma.employee.findMany();
   res.json(employees);
+});
+
+app.post('/stores', async (req, res) => {
+  const name = req.body.name;
+  const newStore = await prisma.store.create({
+    data: {
+      name:  name,
+    },
+  });
+  res.json(newStore);
+});
+
+app.get('/stores', async (req, res) => {
+  const stores = await prisma.store.findMany();
+  res.json(stores);
 });
 
 app.listen(PORT, () => {
